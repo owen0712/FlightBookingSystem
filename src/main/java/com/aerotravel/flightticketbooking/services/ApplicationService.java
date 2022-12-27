9bca3f4fed284e265b7feeef1b7290cffdce7317
Tan Jun Ling <owentan712@gmail.com>
2022-12-27 21:22:59 +0800

    Add Implementation

--- src/main/java/com/aerotravel/flightticketbooking/services/ApplicationService.java
+++ src/main/java/com/aerotravel/flightticketbooking/services/ApplicationService.java
@@ -1,5 +1,12 @@
 package com.aerotravel.flightticketbooking.services;
 
+import java.util.List;
+
+import com.aerotravel.flightticketbooking.model.Application;
+
 public interface ApplicationService {
 
+    public abstract List<Application> getAllApplication();
+    public abstract Application saveApplication(Application application);
+    public abstract void deleteApplicationById(long applicationId);
 }
--- src/main/java/com/aerotravel/flightticketbooking/services/servicesimpl/ApplicationServiceImpl.java
+++ src/main/java/com/aerotravel/flightticketbooking/services/servicesimpl/ApplicationServiceImpl.java
@@ -1,9 +1,32 @@
 package com.aerotravel.flightticketbooking.services.servicesimpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.aerotravel.flightticketbooking.model.Application;
+import com.aerotravel.flightticketbooking.repository.ApplicationRepository;
 import com.aerotravel.flightticketbooking.services.ApplicationService;
 
 @Service
 public class ApplicationServiceImpl implements ApplicationService {
 
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
+    @Override
+    public List<Application> getAllApplication() {
+        return applicationRepository.findAll();
+    }
+
+    @Override
+    public Application saveApplication(Application application) {
+        return applicationRepository.save(application);
+    }
+
+    @Override
+    public void deleteApplicationById(long applicationId) {
+        applicationRepository.deleteById(applicationId);
+    }
 }
